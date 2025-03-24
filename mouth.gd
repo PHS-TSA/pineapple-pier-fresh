0d3f9892f28b479a1324e3a8468a4bbbfc0c4f16
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2025-03-24 17:47:14 -0500

    feat: munch sound

--- mouth.gd
+++ mouth.gd
@@ -2,9 +2,7 @@ extends Area3D
 
 
 func _on_body_entered(body):
-	print("collisions work")
-	print(body)
-	print(body.scene_file_path)
 	if(body.scene_file_path == "res://hotdog.tscn"):
-		print("collisions extra work")
+		%MunchSound.play()
 		body.queue_free()
+		
