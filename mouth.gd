9e5a605e2a74aa50853d31444b5f4a9019ccebd2
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2025-03-24 20:18:17 -0500

    feat: glizzy game complete

--- mouth.gd
+++ mouth.gd
@@ -4,5 +4,7 @@ extends Area3D
 func _on_body_entered(body):
 	if(body.scene_file_path == "res://hotdog.tscn"):
 		%MunchSound.play()
-		body.queue_free()
+		print(body.get_parent())
+		body.get_parent().remove_child(body)
+		
 		
