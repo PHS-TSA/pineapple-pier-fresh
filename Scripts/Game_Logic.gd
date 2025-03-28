7399665723db4c2dd98a5716094f3cd2a2d764af
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2025-03-28 13:32:41 -0500

    feat: player respawn and polishing

--- Scripts/Game_Logic.gd
+++ Scripts/Game_Logic.gd
@@ -2,5 +2,8 @@ extends Node3D
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if(body.name == "XROrigin3D"):
-		%XROrigin3D.global_position = %Spawn.global_position
+	#if(body.name == "PlayerBody"):
+	if(body.name == "PlayerBody"):
+		print(%XROrigin3D.global_position)
+		%XROrigin3D.set_global_position(Vector3(0,0,0)) 
+		print(%XROrigin3D.global_position)
