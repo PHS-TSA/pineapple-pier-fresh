12628da470924a56a8162300d36d3489e5dad13b
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2025-03-27 23:56:40 -0500

    feat: pop sound :(

---
+++ balloon_2.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+
+func _on_area_3d_body_entered(body):
+	if(body.scene_file_path == "res://ball.tscn"):
+		%PopSound.play()
+		self.queue_free()
+		body.queue_free()
--- dart_board.gd
+++ dart_board.gd
@@ -2,6 +2,9 @@ extends Node3D
 
 const BALLOON = preload("res://balloon2.tscn")
 
+#This implementation is terrible but i want to go to bed
+var balloonsLeft
+
 func _ready():
 	#%Balloon.get_child(0).mesh.material.albedo_color = Color(255,255,00)
 	for child in %BalloonMarkers.get_children():
@@ -9,6 +12,8 @@ func _ready():
 		balloon.position = child.position
 		balloon.rotate_x(90)
 		%Balloons2.add_child(balloon)
+	
+	balloonsLeft = len(%Balloons2.get_children())
 	
 	for child in %Balloons2.get_children():	
 		print(child.get_child(0))
@@ -18,3 +23,11 @@ func _ready():
 		var b = randi_range(0,255) / 255.0
 		material.albedo_color = Color(r, g, b)
 		child.get_child(0).set_surface_override_material(0, material)
+	
+		
+func _process(delta):
+	#may god have mercy on my soul for this if statement. TODO rework this pimp
+	if(len(%Balloons2.get_children()) < balloonsLeft):
+		%PopSound.play()
+		balloonsLeft = len(%Balloons2.get_children())
+	
