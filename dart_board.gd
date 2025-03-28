60a63da9bc9f23d1b86757ea058fab9a44e4344a
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2025-03-28 01:01:30 -0500

    feat: more dart pop

--- dart_board.gd
+++ dart_board.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 const BALLOON = preload("res://balloon2.tscn")
+const DART = preload("res://ball.tscn")
 
 '''
 How to finish this:
@@ -16,6 +17,10 @@ var playerWin = false
 
 func _ready():
 	#%Balloon.get_child(0).mesh.material.albedo_color = Color(255,255,00)
+	spawnBalloons()
+	
+
+func spawnBalloons():
 	var i = 0
 	for child in %BalloonMarkers.get_children():
 		var balloon = BALLOON.instantiate()
@@ -30,7 +35,6 @@ func _ready():
 	
 	for child in %Balloons2.get_children():	
 		
-		print(child.name)
 		var material = child.get_child(0).get_surface_override_material(0).duplicate()
 		var r = randi_range(0,255) / 255.0
 		var g = randi_range(0,255) / 255.0
@@ -38,7 +42,30 @@ func _ready():
 		material.albedo_color = Color(r, g, b)
 		child.get_child(0).set_surface_override_material(0, material)
 	
+
+
+func _on_dart_button_button_pressed(button):
+	if(playerWin == true):
+		print(playerWin)
+		playerWin = false
+		print(playerWin)
+		%DartWinLabel.visible = false
+		%InGameLabels.visible = true
+		spawnBalloons()
 		
+		
+	for i in range(len(%Darts.get_children())):
+		%Darts.get_child(i).queue_free()
+	
+	#MOVE THIS SCRIPT TO MILK BOTTLE OBJECT CONTAINER
+	
+	# I should optimize this but state is in 2 weeks :) 
+	
+	for child in %DartMarkers.get_children():
+		var dart = DART.instantiate()
+		dart.position = child.position
+		%Darts.add_child(dart)
+
 func _process(delta):
 	#may god have mercy on my soul for this if statement. TODO rework this pimp
 	if(len(%Balloons2.get_children()) < balloonsLeft):
@@ -47,6 +74,8 @@ func _process(delta):
 		var winningBalloonNode = %Balloons2.get_node_or_null("Balloon" + str(winningBalloon))
 		if winningBalloonNode == null:
 			print("You popped the winning balloon!")
+			%InGameLabels.visible = false
+			%DartWinLabel.visible = true
 			playerWin = true
 			%DartConfetti.emitting = true
 			%DartYay.play()
--- dart_spawning.gd
+++ dart_spawning.gd
@@ -1,19 +1,5 @@
 extends Node3D
 
 #update with dart
-const DART = preload("res://ball.tscn")
 
-
-func _on_dart_button_button_pressed(button):
-	for i in range(len(%Darts.get_children())):
-		%Darts.get_child(i).queue_free()
-	
-	#MOVE THIS SCRIPT TO MILK BOTTLE OBJECT CONTAINER
-	
-	# I should optimize this but state is in 2 weeks :) 
-	
-	for child in %DartMarkers.get_children():
-		var dart = DART.instantiate()
-		dart.position = child.position
-		%Darts.add_child(dart)
 	
